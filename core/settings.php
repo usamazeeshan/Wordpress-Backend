c451333b4ec8fdb110edfc787d371e01c04ffcd6
Sarwar A Kawsr <sakawsar70@gmail.com>
2023-02-21 18:30:17 +0600

    Added instruction

--- core/settings.php
+++ core/settings.php
@@ -43,10 +43,10 @@ if( isset($_POST['roadcube_sync_users']) ) {
 </table>
 <h1><?php _e('Usage documentation','roadcube'); ?></h1>
 <ol>
-    <li>[roadcube_user_login] - Shortcode to show user login UI.</li>
-    <li>[roadcube_show_gifts] - Shortcode to show gifts.</li>
-    <li>[roadcube_get_user_points] - Shortcode to show user points.</li>
-    <li>[roadcube_existing_user_register_form] - Shortcode to show existing user registration form.</li>
+    <li>[roadcube_user_login] - <?php _e('Shortcode to show user login UI.','roadcube'); ?></li>
+    <li>[roadcube_show_gifts] - <?php _e('Shortcode to show gifts.','roadcube'); ?></li>
+    <li>[roadcube_get_user_points] - <?php _e('Shortcode to show user points.','roadcube'); ?></li>
+    <li>[roadcube_existing_user_register_form] - <?php _e('Shortcode to show existing user registration form.','roadcube'); ?></li>
 </ol>
 <?php
 // // update_user_meta(1,'roadcube_mobile','1737008004');
